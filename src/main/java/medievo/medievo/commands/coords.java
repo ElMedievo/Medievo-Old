3cd895c72743eb68c70bc949ef6bbf59343ba181
Jorge Orlando <bgmp0021@gmail.com>
2018-10-23 22:43:32 -0300

    Agregar mensajes a /coords

--- src/main/java/medievo/medievo/commands/coords.java
+++ src/main/java/medievo/medievo/commands/coords.java
@@ -9,6 +9,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import static medievo.medievo.util.generic.noconsole;
+
 public class coords implements CommandExecutor {
 
     private final main plugin;
@@ -20,17 +22,23 @@ public class coords implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("coords")) {
-            if (args.length == 0) {
-                if (sender instanceof Player) {
-                    Player player = (Player) sender;
-                    Location loc = player.getLocation();
-
-                    int x = loc.getBlockX();
-                    int y = loc.getBlockY();
-                    int z = loc.getBlockZ();
-
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                Location loc = player.getLocation();
+                int x = loc.getBlockX();
+                int y = loc.getBlockY();
+                int z = loc.getBlockZ();
+                if (args.length == 0) {
                     Bukkit.broadcastMessage(ChatColor.GOLD + "» " + ChatColor.YELLOW + sender.getName() + ":" + "\nX: " + ChatColor.GREEN + x + ChatColor.YELLOW + "\nY: " + ChatColor.GREEN + y + ChatColor.YELLOW + "\nZ: " + ChatColor.GREEN + z);
+                } else {
+                    StringBuilder locinput = new StringBuilder();
+                    for (int i = 0; i < args.length; i++) {
+                        locinput.append(args[i] + " ");
+                    }
+                    Bukkit.broadcastMessage(ChatColor.GOLD + "» " + ChatColor.YELLOW + sender.getName() + ":" + "\nX: " + ChatColor.GRAY + locinput + ChatColor.GREEN + x + ChatColor.YELLOW + "\nY: " + ChatColor.GREEN + y + ChatColor.YELLOW + "\nZ: " + ChatColor.GREEN + z);
                 }
+            } else {
+                sender.sendMessage(noconsole);
             }
         }
         return true;
