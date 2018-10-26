9bc80e7234073237c9249b52d8819d31ccce8f1a
Jorge Orlando <bgmp0021@gmail.com>
2018-10-25 21:58:36 -0300

    Añadir VIP a OneSip

--- src/main/java/medievo/medievo/events/rankstemp.java
+++ src/main/java/medievo/medievo/events/rankstemp.java
@@ -29,7 +29,7 @@ public class rankstemp implements Listener {
         } else if /*12x2*/(puuid.equals("9e03cc2e-3d48-4dff-b38c-4dc55e011a1d")) {
             player.setPlayerListName(ChatColor.RED + "❖" + ChatColor.YELLOW + player.getName() + ChatColor.RESET);
             player.setDisplayName(ChatColor.RED + "❖" + ChatColor.YELLOW + player.getName() + ChatColor.RESET);
-        } else if /*Bernardita, Caprileee, Ferrada, drakos_*/(puuid.equals("b66a092f-1719-4ed7-8cec-0fb5738ec853") || puuid.equals("fc0c0645-c4a6-407f-b2f0-9cef54b59f27") || puuid.equals("5ecbb50f-560f-4a80-aabe-619da29f5e8a") || puuid.equals("4f89273b-7d68-4173-a3f2-9c4b8451da9b")) {
+        } else if /*Bernardita, Caprileee, Ferrada, drakos_, OneSip*/(puuid.equals("b66a092f-1719-4ed7-8cec-0fb5738ec853") || puuid.equals("fc0c0645-c4a6-407f-b2f0-9cef54b59f27") || puuid.equals("5ecbb50f-560f-4a80-aabe-619da29f5e8a") || puuid.equals("4f89273b-7d68-4173-a3f2-9c4b8451da9b") || puuid.equals("6a239799-5fea-4353-ac51-1ad7046b5c2e")) {
             player.setPlayerListName(ChatColor.DARK_PURPLE + "*" + ChatColor.YELLOW + player.getName() + ChatColor.RESET);
             player.setDisplayName(ChatColor.DARK_PURPLE + "*" + ChatColor.YELLOW + player.getName() + ChatColor.RESET);
         } else if (puuid.equals("2cef6d05-ec9a-44b3-bedc-53359c68ae65")) {
