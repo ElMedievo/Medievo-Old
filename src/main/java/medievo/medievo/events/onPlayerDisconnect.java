2f5ba19d26f611d109f602298bfa368a4048e16b
Jorge Orlando <bgmp0021@gmail.com>
2018-10-29 01:09:26 -0300

    Arreglar error interno de /tpcancel

--- src/main/java/medievo/medievo/events/onPlayerDisconnect.java
+++ src/main/java/medievo/medievo/events/onPlayerDisconnect.java
@@ -2,9 +2,12 @@ package medievo.medievo.events;
 
 import medievo.medievo.main;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import static medievo.medievo.main.tpaqueue;
 
 public class onPlayerDisconnect implements Listener {
 
@@ -16,6 +19,12 @@ public class onPlayerDisconnect implements Listener {
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        if (tpaqueue.containsValue(player)) {
+            tpaqueue.values().remove(player);
+        }
+
         event.setQuitMessage(ChatColor.RED + "« " + event.getPlayer().getDisplayName());
     }
 }
