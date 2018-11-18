d923cca6e2efd590364684e1ab1449afbe297ff3
unknown <bgmp0021@gmail.com>
2018-11-18 13:43:00 -0300

    Delete 1.12 deprecated

--- src/main/java/medievo/medievo/util/generic.java
+++
@@ -1,9 +0,0 @@
-package medievo.medievo.util;
-
-import org.bukkit.ChatColor;
-
-public class generic {
-    public static String noperms = ChatColor.YELLOW + "⚠ " + ChatColor.RED + "You do not have permission";
-    public static String syntaxerror = ChatColor.YELLOW + "⚠ " + ChatColor.RED + "Syntax error!";
-    public static String noconsole = ChatColor.YELLOW + "⚠ " + ChatColor.RED + "Debes ser un jugador para ejecutar este comando";
-}
